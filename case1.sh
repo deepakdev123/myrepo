0bc770caeabb2e767aa4c91271e22f65de139044
deepakdev123 <118237041+deepakdev123@users.noreply.github.com>
2022-11-18 19:23:18 +0530

    Update case1.sh

--- case1.sh
+++ case1.sh
@@ -7,7 +7,7 @@ echo "enter 01 for addition \n enter 02 for subtraction"
 read x
 case $x in
 "01") c=`expr  $a + $b`
-echo "the sum of two numbers is:$c"
+echo "the addition of two numbers is:$c"
 ;;
 "02")  c=`expr  $a - $b`
 echo "the diff of two numbers is:$c"
